7a6b76aba956dbcae313953f822e268c91787958
Marina Veldhuizen <marina.veldhuizen@student.fontys.nl>
2024-06-19 14:17:44 +0200

    feat(development): initial commit

---
+++ src/app/compontents/Navigation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+
+    return (
+        <nav>
+            <div>
+                <button>
+Test
+                </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navigation;
